ae0bb6dc1caff08679855fc52d69f3b5258ce984
vasanth <jvasanthirtt05@gmail.com>
2019-09-07 15:26:41 +0530

    16

--- csv_to_avro_conversion/conversion.py
+++ csv_to_avro_conversion/conversion.py
@@ -3,26 +3,26 @@ import csv
 
 
 schema = {"namespace": "csv to avro",
-		  "type": "record",
-		  "name": "csv",
-		  "fields": [
-			  {"name": "id", "type": "int", "default":0},
-			  {"name": "name",  "type": "string", "default":"aaa"},
-			  {"name": "lastname", "type": "string", "default":"cccc"}
-		  ]
-		  }
+	  "type": "record",
+	  "name": "csv",
+	  "fields": [
+		  {"name": "id", "type": "int", "default":0},
+		  {"name": "name",  "type": "string", "default":"aaa"},
+		  {"name": "lastname", "type": "string", "default":"cccc"}
+	  ]
+	  }
 parsed_schema = parse_schema(schema)
 
 csv_inp = "/home/vasanth/git-repo_home/csv_to_avro_conversion/csv_to_avro_conversion/source/input.csv"
 #avro_schema = r"C:\Users\<directory>\output_avro_schema.avsc"
 avro_output = "/home/vasanth/git-repo_home/csv_to_avro_conversion/csv_to_avro_conversion/target/output.avro"
 
-records=[]
+records = []
 with open(csv_inp, 'r') as csvinp:
-	rdr = csv.reader(csvinp)
-	header = next(rdr)
-	for row in rdr:
-		records.append((dict(zip(header, [int(row[0]), row[1], row[2]]))))
+    rdr = csv.reader(csvinp)
+    header = next(rdr)
+    for row in rdr:
+        records.append((dict(zip(header, [int(row[0]), row[1], row[2]]))))
 
 
 print(records)
